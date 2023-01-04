8b2279469fb5a42812f8ad52f44bf4394f877f31
Kanha Tomar <kanharajput91@gmail.com>
2023-01-04 20:39:16 +0530

    register model to work with it through admin panel

--- BookOutlet/admin.py
+++ BookOutlet/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+# now the book model is registered and we can see it now in admin panel
+# also we can edit previously entered data and also we can add some new entries to it
+admin.site.register(Book) 
