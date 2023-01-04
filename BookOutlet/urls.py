55afaca164da45e4138f3015f76c4ea85f7d4b07
Kanha Tomar <kanharajput91@gmail.com>
2023-01-04 15:35:16 +0530

    detailed page is added

--- BookOutlet/urls.py
+++ BookOutlet/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('',views.showBooKList)
+    path('',views.showBooKList),
+    path('<int:id>',views.detailPage)         # id is the slug for url
 ]
--- BookOutlet/views.py
+++ BookOutlet/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Book
 
 # Create your views here.
@@ -6,3 +6,12 @@ from .models import Book
 def showBooKList(request):
     Books = Book.objects.all()
     return render(request,'BookOutlet/home.html',{'Books':Books})
+
+
+def detailPage(request,id):
+    # book = Book.objects.all().get(pk=id)                 # to fetch id from database we can use id or pk(primary key)
+    # if id not match it push and exception "Book matching query does not exist."
+    # so we have to handle this exception and return 404 page 
+    # use method get_object_or_404 method, if object found, return it to book object or it automatically 404 error.
+    book = get_object_or_404(Book, pk=id)
+    return render(request,'BookOutlet/detail.html',{'book':book})
