df13f060758e28b2e97d0bd0abcbd9d599bdb4ef
Kanha Tomar <kanharajput91@gmail.com>
2023-01-04 15:45:09 +0530

    dtl-url to create url of pages

--- BookOutlet/urls.py
+++ BookOutlet/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.showBooKList),
-    path('<int:id>',views.detailPage)         # id is the slug for url
+    path('<int:id>',views.detailPage, name="detail-url")         # id is the slug for url
 ]
