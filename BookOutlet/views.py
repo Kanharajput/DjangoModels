fb2f28b49f8e7f165f75f9159cf671606af8f316
Kanha Tomar <kanharajput91@gmail.com>
2023-01-04 19:00:28 +0530

    aggregate functios

--- BookOutlet/views.py
+++ BookOutlet/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Book
+from django.db.models import Avg, Min
 
 # Create your views here.
 
 def showBooKList(request):
-    Books = Book.objects.all()
-    return render(request,'BookOutlet/home.html',{'Books':Books})
+    Books = Book.objects.all().order_by("rating")      #order_by("-rating") order the data in descending order
+    no_of_books = Books.count()           # return the count of books/ entries in the database 
+    aggre_dict = Books.aggregate(Avg("rating"),Min("rating"))           # aggregate return a dictionary so that multiple operation can perform via a single statement
+    return render(request,'BookOutlet/home.html',{
+        'Books':Books,
+        'no_of_books':no_of_books,
+        'aggre_dict':aggre_dict
+        })
 
 
 def detailPage(request,slug_in_url):
