fd199f643c624fee29e2f3f4e03ce8aa2e09d98f
Kanha Tomar <kanharajput91@gmail.com>
2023-01-04 14:47:02 +0530

    fetched data and pass to template

--- BookOutlet/views.py
+++ BookOutlet/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
 
 def showBooKList(request):
-    return render(request,'BookOutlet/home.html')
+    Books = Book.objects.all()
+    return render(request,'BookOutlet/home.html',{'Books':Books})
